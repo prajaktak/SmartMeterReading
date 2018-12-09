9a3f3a728a546875f1a674fa7c529e9514d2848a
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-12-09 22:41:43 +0100

    Initial Commit

---
+++ SmartMeterReadingTests/SmartMeterReadingTests.swift
@@ -0,0 +1,34 @@
+//
+//  SmartMeterReadingTests.swift
+//  SmartMeterReadingTests
+//
+//  Created by Prajakta Kulkarni on 09/12/2018.
+//  Copyright © 2018 Prajakta Kulkarni. All rights reserved.
+//
+
+import XCTest
+@testable import SmartMeterReading
+
+class SmartMeterReadingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
